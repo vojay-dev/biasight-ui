0f2f40441a27efd192b14d9885dd1d5be419fdf7
Volker Janz <volker.janz@innogames.com>
2024-10-19 16:18:53 +0200

    Add icons, improve user flow by adding more navigatio buttons

--- src/components/Analyze.vue
+++ src/components/Analyze.vue
@@ -251,6 +251,28 @@
 
           </div>
         </div>
+
+        <div class="flex w-full flex-col">
+          <div class="divider divider-warning">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-14">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M15.042 21.672 13.684 16.6m0 0-2.51 2.225.569-9.47 5.227 7.917-3.286-.672Zm-7.518-.267A8.25 8.25 0 1 1 20.25 10.5M8.288 14.212A5.25 5.25 0 1 1 17.25 10.5" />
+            </svg>
+
+            Actions
+          </div>
+        </div>
+
+        <div class="flex flex-row justify-center pt-2 pb-2">
+          <div>
+            <router-link to="/" tag="button" class="btn btn-primary btn-outline w-48">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M21 16.811c0 .864-.933 1.406-1.683.977l-7.108-4.061a1.125 1.125 0 0 1 0-1.954l7.108-4.061A1.125 1.125 0 0 1 21 8.689v8.122ZM11.25 16.811c0 .864-.933 1.406-1.683.977l-7.108-4.061a1.125 1.125 0 0 1 0-1.954l7.108-4.061a1.125 1.125 0 0 1 1.683.977v8.122Z" />
+              </svg>
+
+              Start again
+            </router-link>
+          </div>
+        </div>
       </div>
     </div>
   </div>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -16,12 +16,20 @@
                 <p class="mb-5">BiaSight analyzes websites for gender bias, providing feedback and suggestions using <span class="badge badge-warning badge-outline tooltip tooltip-bottom" data-tip="Using Google Gemini">AI</span>.</p>
                 <div class="flex flex-wrap gap-4 items-center justify-center">
                   <div>
-                    <router-link to="/init" tag="button" class="btn btn-primary btn-outline w-48">
+                    <router-link to="/init" tag="button" class="btn btn-success btn-outline w-48">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z" />
+                      </svg>
+
                       Get started
                     </router-link>
                   </div>
                   <div>
-                    <router-link to="/init" tag="button" class="btn btn-success btn-outline w-48">
+                    <router-link to="/init" tag="button" class="btn btn-primary btn-outline w-48">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5" />
+                      </svg>
+
                       Learn More
                     </router-link>
                   </div>
