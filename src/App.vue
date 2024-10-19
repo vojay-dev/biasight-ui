73188c13d5f48db5dc12c793a33dc18f4fc1a4dd
Volker Janz <volker.janz@innogames.com>
2024-10-19 16:21:44 +0200

    Replace BiaSight menu icon

--- src/App.vue
+++ src/App.vue
@@ -51,7 +51,10 @@
         </div>
         <div class="navbar-center">
           <router-link to="/" tag="button" class="btn btn-ghost text-lg gemini" v-bind:class="{ 'gemini-light': ['Home'].includes(currentRouteName) }">
-            <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24"><g fill="#fff"><path d="M4.99 12.862a7.1 7.1 0 0 0 12.171 3.924a2 2 0 0 1-.156-.637L17 16l.005-.15a2 2 0 1 1 1.769 2.137a9.099 9.099 0 0 1-15.764-4.85a1 1 0 0 1 1.98-.275"/><path d="M12 8a4 4 0 1 1-3.995 4.2L8 12l.005-.2A4 4 0 0 1 12 8"/><path d="M13.142 3.09a9.1 9.1 0 0 1 7.848 7.772a1 1 0 0 1-1.98.276a7.1 7.1 0 0 0-6.125-6.064A7.1 7.1 0 0 0 6.837 7.21a2 2 0 1 1-3.831.939L3 8l.005-.15a2 2 0 0 1 2.216-1.838a9.1 9.1 0 0 1 7.921-2.922"/></g></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#fff" class="size-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+            </svg>
+
             BiaSight
           </router-link>
         </div>
